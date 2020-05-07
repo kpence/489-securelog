cead1ab33bb92dc26889a7fab96e8f664cbd33f1
kpence <pencekyle@gmail.com>
2020-05-07 01:05:04 +0000

    proof

--- hospitalstate.h
+++ hospitalstate.h
@@ -1,12 +1,38 @@
+#ifndef HOSPITALSTATE_H_
+#define HOSPITALSTATE_H_
+/*
+MIT License
+
+Copyright (c) 2020 Kyle Pence
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #include <map>
 #include <vector>
 #include <string>
 #include <utility>
 #include <iostream>
 
-#include "EntryParser.h"
-#include "FileReaderWriter.h"
-#include "error.h"
+#include "./EntryParser.h"
+#include "./FileReaderWriter.h"
+#include "./error.h"
 
 #define ERROR_EXIT_CODE 255
 
@@ -176,3 +202,6 @@ int get_hospital_state(FileReaderWriter& frw, EntryParser& p, map<pair<char,stri
   rooms.pop_back();
   return 1;
 }
+
+
+#endif // HOSPITALSTATE_H_
